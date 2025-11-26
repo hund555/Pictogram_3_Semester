08da0b23b742572c6c67bb484c94ce1847b8bc8f
Sunisima <lsun8525@gmail.com>
2025-11-26 13:34:27 +0100

    Starting on method to create new users

---
+++ react_webapplication/src/Domain/CreateUserWeb.ts
@@ -0,0 +1,10 @@
+/*
+A Model class for the users
+*/
+export default interface CreateUserWeb {
+    userId: string;
+    email: string;
+    password: string;
+    fullName: string;
+    role: string;
+}
---
+++ react_webapplication/src/Services/UserService.ts
@@ -0,0 +1,15 @@
+import type CreateUserWeb from "../Domain/CreateUserWeb";
+import axios, { type AxiosResponse } from "axios";
+
+class UserService {
+    
+
+    // Creates a new user in the system
+    static async createUser(email: string, fullName: string, password: string): Promise<CreateUserWeb> {
+        const baseURL = "http://localhost:8080";
+        const payload = { email, fullName, password };
+
+        return axios.post(baseURL + "/users", payload).then((response: AxiosResponse<CreateUserWeb>) => response.data);
+    }
+}
+export default UserService;
