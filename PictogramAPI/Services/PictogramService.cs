6178350bcfaae78f007d88ba43543e7138681034
hund555 <allanholmiversen@gmail.com>
2025-11-27 10:34:33 +0100

    Added pictogram service, need methods. renamed dto folder. added collection name for pictograms in databaseinfo and appsettings

---
+++ PictogramAPI/Services/PictogramService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using PictogramAPI.Domain;
+
+namespace PictogramAPI.Services
+{
+    public class PictogramService
+    {
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<Pictogram> _pictogramsCollection;
+
+        public PictogramService(IOptions<DatabaseInfo> options)
+        {
+            MongoClient mongoClient = new MongoClient(options.Value.ConnectionString);
+            _database = mongoClient.GetDatabase(options.Value.DatabaseName);
+            _pictogramsCollection = _database.GetCollection<Pictogram>(options.Value.PictogramCollectionName);
+        }
+
+        public async Task CreatePictogram()
+        {
+
+        }
+    }
+}
