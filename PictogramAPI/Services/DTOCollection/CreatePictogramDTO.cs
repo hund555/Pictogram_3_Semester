39874ad6449f75b97f7b1a2d40f1459abfe053ab
hund555 <allanholmiversen@gmail.com>
2025-11-27 10:38:51 +0100

    Added CreatePictogramDTO need ajustment for how to recieve picture

---
+++ PictogramAPI/Services/DTOCollection/CreatePictogramDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PictogramAPI.Services.DTOCollection
+{
+    public class CreatePictogramDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string FileType { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
