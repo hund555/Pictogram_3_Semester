ff97bccad1b1d11beb0f335130f3cf28f17cf448
Sunisima <lsun8525@gmail.com>
2025-12-04 13:58:01 +0100

    Started UI

--- WPF_PictoPlanner_Admin/Commands/RelayCommand.cs
+++ WPF_PictoPlanner_Admin/Commands/RelayCommand.cs
@@ -1,6 +1,22 @@
-﻿namespace WPF_PictoPlanner_Admin.Commands
+﻿using System.Windows.Input;
+
+namespace WPF_PictoPlanner_Admin.Commands
 {
-    public class RelayCommand
+    /// <summary>
+    /// A command to relay its functions 
+    /// </summary>
+    public class RelayCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
