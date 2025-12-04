50fb8e4984a16847fce4259caeb9000ed769c0f9
Sunisima <lsun8525@gmail.com>
2025-12-04 09:44:10 +0100

    Created folders for the project

---
+++ WPF_PictoPlanner_Admin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_PictoPlanner_Admin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
