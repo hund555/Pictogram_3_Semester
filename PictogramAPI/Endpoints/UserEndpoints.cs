5e96a27c69ea355b3658a6c6f4f91011b388b870
hund555 <allanholmiversen@gmail.com>
2025-12-09 12:28:14 +0100

    testing

--- PictogramAPI/Endpoints/UserEndpoints.cs
+++ PictogramAPI/Endpoints/UserEndpoints.cs
@@ -80,8 +80,7 @@ namespace PictogramAPI.Endpoints
             })
             .WithTags("Users")
             .WithName("LogoutUser")
-            .WithSummary("Logout the current user.")
-            .RequireAuthorization();
+            .WithSummary("Logout the current user.");
 
             app.MapGet("/users/getusers", async (IUserService userService) =>
             {
