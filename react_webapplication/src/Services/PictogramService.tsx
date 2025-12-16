f50fb0f0f9a12a12f5f311ea05588c86a7c06be0
Sunisima <lsun8525@gmail.com>
2025-12-16 09:15:36 +0100

    Changes to URL

--- react_webapplication/src/Services/PictogramService.tsx
+++ react_webapplication/src/Services/PictogramService.tsx
@@ -4,15 +4,17 @@ import axios, { type AxiosResponse } from 'axios';
 import Environment from "../Utillity";
 
 const baseurl = Environment.getBackendAddress();
-class PictogramService {
-    static async createPictogram(title: string, description:string, fileType:string, isPrivate:boolean, picture: string) : Promise<Pictogram> { 
+class PictogramService
+{
+    static async createPictogram(title: string, description: string, fileType: string, isPrivate: boolean, picture: string): Promise<Pictogram>
+    { 
        
     
 
         const payload = { title, description, fileType, isPrivate, picture};
 
         console.log(JSON.stringify(picture));
-        return axios.post<Pictogram>("http://localhost:8080/pictograms", payload, {withCredentials: true})
+        return axios.post<Pictogram>("http://localhost:8080/pictograms/create", payload, {withCredentials: true})
         .then((response: AxiosResponse<Pictogram>) => response.data)
          
             //.catch(function (error) { console.log(error) })
@@ -20,22 +22,21 @@ class PictogramService {
 
     static async displayAllPictograms() : Promise<AllPictograms[]>
     {
-            return axios.get<AllPictograms[]>(`http://localhost:8080/pictograms`, { withCredentials: true })
+            return axios.get<AllPictograms[]>(`http://localhost:8080/pictograms/getAllPictograms`, { withCredentials: true })
             .then((response: AxiosResponse<AllPictograms[]>) => response.data)
     }
 
 
-    static async getAllPictograms(userid: string): Promise<Pictogram[]> {
+    static async getAllPictograms(userid: string): Promise<Pictogram[]>
+    {
 
-        return new Promise<Pictogram[]>((resolve, reject) => {
+        return new Promise<Pictogram[]>((resolve, reject) =>
+        {
             axios.get<Pictogram[]>(baseurl + "/pictograms", { data: { user_id: userid } })
                 .then(res => resolve(res.data))
                 .catch(err => reject(err));
         });
-
-
     }
-
 }
 
 export default PictogramService;
