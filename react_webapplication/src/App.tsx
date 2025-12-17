ef0097ad94a82aec0c0a7a9e33102287a7ecb49a
00lev1athan <somenrullendesten@gmail.com>
2025-12-17 15:15:18 +0100

    .

--- react_webapplication/src/App.tsx
+++ react_webapplication/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import LoginPage from "./UI/LoginPage";
 import RegisterNewUser from "./UI/RegisterNewUser";
 import './App.css'
